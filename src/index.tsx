754a4602bf6b05a4a675d6c04b4d0a59d99b1cd6
abdullahmorrison <abdullahmorrison@gmail.com>
2022-05-28 14:09:28 -0400

    reverted back to a single html file

--- src/index.tsx
+++ src/index.tsx
@@ -4,24 +4,7 @@ import './index.css';
 import App from './components/App';
 import reportWebVitals from './reportWebVitals';
 
-let root: ReactDOM.Root
-
-// load component based on current view (viewer/config etc.)
-const rootPanel = document.getElementById("root-panel") as HTMLElement
-const rootConfig = document.getElementById("root-config") as HTMLElement
-
-if(rootPanel) {
-  root = ReactDOM.createRoot(
-    rootPanel
-  )
-}else if(rootConfig) {
-  root = ReactDOM.createRoot(
-    rootConfig
-  )
-}else{
-  root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement)
-  console.error("No root element found")
-}
+const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement)
 
 root.render(
   <React.StrictMode>

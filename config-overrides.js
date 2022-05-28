c2f61555d24716e570c57a2f80a48c704f9d38c5
abdullahmorrison <abdullahmorrison@gmail.com>
2022-05-28 19:35:56 -0400

    added multiple entry points

---
+++ config-overrides.js
@@ -0,0 +1,22 @@
+const multipleEntry = require('react-app-rewire-multiple-entry')([
+  {
+    entry: 'src/index.tsx',
+    template: 'public/index.html',
+    outPath: '/panel.html',
+  },
+  {
+    entry: 'src/pages/config/index.tsx',
+    template: 'public/index.html',
+    outPath: '/config.html',
+  }
+]);
+
+module.exports = {
+  webpack: function(config, env) {
+    multipleEntry.addMultiEntry(config)
+    config.optimization = {
+      minimize: false,
+    }
+    return config
+  }
+}

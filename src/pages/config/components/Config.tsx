5f1ab3528125ff6928b36b0df170ba0123e073ba
abdullahmorrison <t-amorrison@microsoft.com>
2022-05-31 19:15:25 -0400

    config page set up

--- src/pages/config/components/Config.tsx
+++ src/pages/config/components/Config.tsx
@@ -1,5 +1,28 @@
+import styles from './config.module.css'
+
+import AnimalData from '../../../assets/animals.json'
+import AnimalPreview from '../../panel/components/animalPreview/AnimalPreview'
+
 export default function Config() {
   return (
-    <div>Config</div>
+    <div className={styles.config}>
+      <h1>Manage Your Ambassadors</h1>
+      <p>Click an ambassador to edit or delete it.</p>
+      <button className={styles.newAmbassadorButton}>+ New Ambassador</button>
+      <div className={styles.animalList}>
+        {
+          AnimalData.map(animal => (
+            <AnimalPreview 
+              key={animal.name}
+              name={animal.name}
+              animalType={animal.animalType}
+              imgSrc={animal.imgSrc}
+              imgAltText={animal.imgAltText}
+            />
+          ))
+        }
+
+      </div>
+    </div>
   )
 }

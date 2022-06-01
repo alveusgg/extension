498b825ff398c7b6f7a6f1e61b7cfe1120171efe
abdullahmorrison <t-amorrison@microsoft.com>
2022-06-01 18:48:54 -0400

    set up animal editor form

--- src/pages/config/components/editorForm/EditorForm.tsx
+++ src/pages/config/components/editorForm/EditorForm.tsx
@@ -3,10 +3,11 @@ import styles from './editorForm.module.css';
 export default function EditorForm() {
   return (
       <div className={styles.editForm}>
-        <div>
+        <div className={styles.imgEdit}>
+            <img src="" alt="img" />
             <input type="file" name="img" id="img" />
         </div>
-        <form>
+        <form className={styles.form}>
             <label htmlFor="name">Name</label>
             <input type="text" name="name" id="name" />
 
@@ -19,17 +20,26 @@ export default function EditorForm() {
             <label htmlFor="iucn">IUCN Status</label>
             <input type="text" name="iucn" id="iucn" />
 
-            <div>
-                <label htmlFor="sex">Sex</label>
-                <select name="sex" id="sex">
-                    <option value="Unknown">Unknown</option>
-                    <option value="Male">Male</option>
-                    <option value="Female">Female</option>
-                </select>
-
-                <label htmlFor="dateofbirth">Date of Birth</label>
-                <input type="date" name="dateofbirth" id="dateofbirth" />
+            <div className={styles.horizontalInput}>
+                <div className={styles.smallInput}>
+                    <label htmlFor="sex">Sex</label>
+                    <select name="sex" id="sex">
+                        <option value="Unknown">Unknown</option>
+                        <option value="Male">Male</option>
+                        <option value="Female">Female</option>
+                    </select>
+                </div>
+                <div className={styles.smallInput}>
+                    <label htmlFor="dateofbirth">Date of Birth</label>
+                    <input type="date" name="dateofbirth" id="dateofbirth" />
+                </div>
             </div>
+
+            <label htmlFor="story">Story</label>
+            <textarea name="story" id="story" cols={30} rows={5}/>
+
+            <label htmlFor="conservationmission">Conservation Mission</label>
+            <textarea name="conservationmission" id="conservationmission" cols={30} rows={5}/>
         </form>
       </div>
   )

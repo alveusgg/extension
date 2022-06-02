f695f37fd76995a17866c4d2510da27a21de9dc3
abdullahmorrison <t-amorrison@microsoft.com>
2022-06-02 09:30:54 -0400

    early animal editor set up

--- src/pages/config/components/animalEditor/AnimalEditor.tsx
+++ src/pages/config/components/animalEditor/AnimalEditor.tsx
@@ -1,11 +1,38 @@
 import styles from './animalEditor.module.css';
 
 import EditorForm from '../editorForm/EditorForm';
+import AnimalCard from '../../../../utils/animalCard/AnimalCard';
+import AnimalButton from '../../../panel/components/animalButton/AnimalButton';
 
 export default function AnimalEditor() {
   return (
     <div className={styles.animalEditor}>
-      <EditorForm/>
+      <div className={styles.editor}>
+        <EditorForm/>
+        <div className={styles.buttons}>
+          <button className={styles.delete}>Delete</button>
+          <div className={styles.mainButtons}>
+            <button className={styles.save}>Save</button>
+            <button className={styles.cancel}>Cancel</button>
+          </div>
+        </div>
+      </div>
+      <div className={styles.preview}>
+        <AnimalCard
+          name="Georgie" 
+          animalType='African Bullfrog'
+          imgSrc=''
+          imgAltText=''
+          close={() => {}}
+        /> 
+        <AnimalButton
+          name="Georgie"
+          animalType='African Bullfrog'
+          imgSrc=''
+          imgAltText=''
+          expand={() => {}}
+        />
+      </div>
     </div>
   )
 }

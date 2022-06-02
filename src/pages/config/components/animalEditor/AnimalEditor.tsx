f5439607e652940c8e6d1230074f3962991073cb
abdullahmorrison <t-amorrison@microsoft.com>
2022-06-02 17:59:20 -0400

    added button icons to buttons

--- src/pages/config/components/animalEditor/AnimalEditor.tsx
+++ src/pages/config/components/animalEditor/AnimalEditor.tsx
@@ -4,16 +4,30 @@ import EditorForm from '../editorForm/EditorForm';
 import AnimalCard from '../../../../utils/animalCard/AnimalCard';
 import AnimalButton from '../../../panel/components/animalButton/AnimalButton';
 
+//icons
+import deleteIcon from '../../../../assets/buttonIcons/delete.svg';
+import saveIcon from '../../../../assets/buttonIcons/save.svg';
+import cancelIcon from '../../../../assets/buttonIcons/cancel.svg';
+
 export default function AnimalEditor() {
   return (
     <div className={styles.animalEditor}>
       <div className={styles.editor}>
         <EditorForm/>
         <div className={styles.buttons}>
-          <button className={styles.delete}>Delete</button>
+          <button className={styles.delete}>
+            <img src={deleteIcon} alt="Delete Icon"/> 
+            Delete
+          </button>
           <div className={styles.mainButtons}>
-            <button className={styles.save}>Save</button>
-            <button className={styles.cancel}>Cancel</button>
+            <button className={styles.save}>
+              <img src={saveIcon} alt="Save Icon"/> 
+              Save
+            </button>
+            <button className={styles.cancel}>
+              <img src={cancelIcon} alt="Cancel Icon"/> 
+              Cancel
+            </button>
           </div>
         </div>
       </div>

699b0f86f89d986f4cd470e0519630950e7b91e7
abdullahmorrison <t-amorrison@microsoft.com>
2022-07-20 19:09:37 -0400

    created loading spinner

---
+++ src/utils/loadingSpinner/LoadingSpinner.tsx
@@ -0,0 +1,9 @@
+import styles from './loadingSpinner.module.css'
+
+export default function LoadingSpinner() {
+  return (
+    <div className={styles.container}>
+        <div className={styles.loading}></div>
+    </div>
+  )
+}

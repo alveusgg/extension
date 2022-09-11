d2128e2232a513918c110581399ca400dbe01875
abdullahmorrison <abdullahmorrison@gmail.com>
2022-09-11 17:23:20 -0400

    removed editmode bug

--- src/pages/config/App.tsx
+++ src/pages/config/App.tsx
@@ -33,7 +33,6 @@ export default function App() {
       ...animalCard,
       [inputProperty]: inputValue
     })
-    setEditMode('update')
   }
   function handleEditImg(event: ChangeEvent<HTMLInputElement>): void{
     const target = event.target as HTMLInputElement;

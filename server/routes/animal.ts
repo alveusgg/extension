2c45b175b2a4f47dc9b6f08f188e5528ed08661b
abdullahmorrison <abdullahmorrison@gmail.com>
2022-10-25 15:46:05 -0400

    update multer settings

--- server/routes/animal.ts
+++ server/routes/animal.ts
@@ -2,11 +2,7 @@ import express from 'express'
 import multer from 'multer'
 
 const storage = multer.diskStorage({
-    destination: (req, file, cb) => {
-        cb(null, 'uploads/')
-    }
-    , filename: (req, file, cb) => {
-        console.log("file: ", file);
+    filename: (req, file, cb) => {
         cb(null, file.originalname)
     }
 })

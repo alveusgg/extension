7b32989aeae8009ae61954df55348dda51334a6a
abdullahmorrison <abdullahmorrison@gmail.com>
2022-12-23 15:34:02 -0500

    added mobile version

--- config-overrides.js
+++ config-overrides.js
@@ -5,6 +5,11 @@ const multipleEntry = require('react-app-rewire-multiple-entry')([
     outPath: 'panel.html',
   },
   {
+    entry: 'src/index.tsx',
+    template: 'public/index.html',
+    outPath: 'mobile.html',
+  },
+  {
     entry: 'src/pages/overlay/index.tsx',
     template: 'public/index.html',
     outPath: 'video_overlay.html',

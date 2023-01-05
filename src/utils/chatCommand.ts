5a8edb7db7fe49763e0e79ea1be9945f65ae87ba
abdullahmorrison <abdullahmorrison@gmail.com>
2023-01-04 19:26:15 -0500

    set up chat command being used by overlay and ambassadorList

---
+++ src/utils/chatCommand.ts
@@ -0,0 +1,10 @@
+import { useEffect, useState } from 'react'
+
+export default function useChatCommand() {
+    const [command, setCommand] = useState()
+
+    useEffect(() => {
+    }, [])
+
+    return command
+}

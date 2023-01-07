52577d53f1eaf840c98ff2e1d81c9fcbf22b821b
abdullahmorrison <abdullahmorrison@gmail.com>
2023-01-07 13:10:42 -0500

    updated chat command hook to handle ambassador names with diacritics

--- src/utils/chatCommand.ts
+++ src/utils/chatCommand.ts
@@ -2,11 +2,45 @@ import { useEffect, useState } from 'react'
 import tmi, { ChatUserstate } from 'tmi.js'
 import AmbassadorData from '../assets/animals.json'
 
+/**
+ * @description Some ambassadors have names with diacritics in them (Ex: Jalapeño). 
+ * Note: a diacritic is a mark added to a letter to change its sound or meaning (Ex: ñ). 
+ * This function creates a map of an ambassador with diacritics in their name and their name without diacritics.
+ * (Ex: { jalapeno: jalapeño })
+ * @returns a map of the normalized names and original names
+ */
+const getMapOfAmbassadorWithDiacritics = (): Map<string, string> => {
+    //store names that have letters with diacritics in them
+    const ambassadorsWithDiacriticsInNames = AmbassadorData.filter(
+        (ambassador) =>{ 
+            const ambassadorOriginalFirstName = ambassador.name.split(' ')[0].toLowerCase()
+            const ambassadorFirstNameWithRemovedDiacritic = ambassadorOriginalFirstName.normalize('NFD').replace(/[\u0300-\u036f]/g, '')
+
+            return ambassadorOriginalFirstName !== ambassadorFirstNameWithRemovedDiacritic
+        }
+    )
+    // a hashmap of the normalized names and their original names
+    const diacriticMap = new Map<string, string>()
+    ambassadorsWithDiacriticsInNames.forEach((ambassador) => {
+        const ambassadorOriginalFirstName = ambassador.name.split(' ')[0].toLowerCase()
+        const ambassadorFirstNameWithRemovedDiacritic = ambassadorOriginalFirstName.normalize('NFD').replace(/[\u0300-\u036f]/g, '')
+        diacriticMap.set(ambassadorFirstNameWithRemovedDiacritic, ambassadorOriginalFirstName)
+    })
+
+    return diacriticMap
+}
+
 export default function useChatCommand() {
     const [command, setCommand] = useState<string>()
     const ambassadorNames = AmbassadorData.map((ambassador) => ambassador.name.split(' ')[0].toLowerCase())
 
-    const client = new tmi.Client({channels: ['AbdullahMorrison']})
+    const diacriticsMap: Map<string, string> = getMapOfAmbassadorWithDiacritics()
+
+    const client = new tmi.Client({channels: [
+        'AbdullahMorrison',
+        'Maya',
+        'AlveusSanctuary'
+    ]})
 
     useEffect(() => {
         client.on('message', messageHandler)
@@ -14,13 +48,17 @@ export default function useChatCommand() {
         client.connect()
     }, [])
 
-    const messageHandler = (target: string, context: ChatUserstate, msg: string, self: boolean) => {
-        if (self) return
+    const messageHandler = (channel: string, tags: ChatUserstate, msg: string, self: boolean) => {
+        // Ignore echoed messages (messages sent by the bot) and messages that don't start with '!'
+        if (self || !msg.trim().startsWith('!')) return
 
-        const commandName = msg.trim().toLowerCase()
-        if(commandName.startsWith('!') && ambassadorNames.find((name) => name === commandName.slice(1))) {
+        const commandName = msg.trim().toLowerCase() 
+        if(ambassadorNames.find((name) => name === commandName)) {
             setCommand(commandName)
+        }else if(diacriticsMap.has(commandName)) { // Check if a user typed a name without diacritics (Ex: !jalapeno should be !Jalapeño)
+            setCommand(diacriticsMap.get(commandName)!)
         }
+
     }
     const connectedHandler = () => {
         console.log('*Twitch extension is connected to chat*')

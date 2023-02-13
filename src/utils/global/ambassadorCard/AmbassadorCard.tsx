7694d35844c02ca74237a56c987bb6875cd270a4
abdullahmorrison <abdullahmorrison@gmail.com>
2023-02-12 19:59:28 -0500

    set up card flp

--- src/utils/global/ambassadorCard/AmbassadorCard.tsx
+++ src/utils/global/ambassadorCard/AmbassadorCard.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react"
 import Ambassador from "../../compositions/ambassador/Ambassador"
 import {calculateAge, formatDate, isBirthday} from "../../dateManager"
 
@@ -23,54 +24,75 @@ export interface AmbassadorCardProps {
 }
 
 export default function AmbassadorCard(props: AmbassadorCardProps) {
+  const [isFlipped, setIsFlipped] = useState(false)
+
   return (
-    <Ambassador ClassName={`${styles.ambassadorCard} ${props.ClassName} ${isBirthday(props.cardData.dateOfBirth) === true ? styles.birthday : ""}`}>
-      {props.close ? (
-        <div className={styles.close} onClick={props.close}>&times;</div>
-      ) : null}
+    <div className={`${styles.ambassador} ${props.ClassName}`}>
+      <Ambassador ClassName={`${styles.ambassadorCard} ${isFlipped ? styles.flipCard : null} ${isBirthday(props.cardData.dateOfBirth) === true ? styles.birthday : ""}`}>
+        <div className={styles.front}>
+          {props.close ? (
+            <div className={styles.close} onClick={props.close}>&times;</div>
+          ) : null}
 
-      <h2 className={styles.name}>{props.cardData.name}</h2>
-      <img className={styles.img} src={props.cardData.img.src} alt={props.cardData.img.altText} />
+          <h2 className={styles.name}>{props.cardData.name}</h2>
+          <img className={styles.img} src={props.cardData.img.src} alt={props.cardData.img.altText} />
 
-      <div className={styles.row}>
-        <h3>Species</h3>
-        <p>{props.cardData.species}</p>
-        <p><i>{props.cardData.scientificName}</i></p>
-      </div>
+          <div className={styles.row}>
+            <h3>Species</h3>
+            <p>{props.cardData.species}</p>
+            <p><i>{props.cardData.scientificName}</i></p>
+          </div>
 
-      <div className={`${styles.row} ${styles.compact}`}>
-        <div>
-          <h3>Sex</h3>
-          <p>{props.cardData.sex}</p>
+          <div className={`${styles.row} ${styles.compact}`}>
+            <div>
+              <h3>Sex</h3>
+              <p>{props.cardData.sex}</p>
+            </div>
+            <div>
+              <h3>Age</h3>
+              <p>
+                {props.cardData.dateOfBirth !== "" ? calculateAge(props.cardData.dateOfBirth) : "Unknown"}
+              </p>
+            </div>
+            <div>
+              <h3>Birthday</h3>
+              <p>
+                {props.cardData.dateOfBirth !== "" ? formatDate(props.cardData.dateOfBirth) : "Unknown"}
+              </p>
+            </div>
+          </div>
+
+          <div className={styles.row}>
+            <h3>IUCN Status</h3>
+            <p>{props.cardData.iucnStatus}</p>
+          </div>
+
+          <div className={`${styles.row} ${styles.story}`}>
+            <h3>Story</h3>
+            <p>{props.cardData.story}</p>
+          </div>
+
+          <div className={`${styles.row} ${styles.conservationMission}`}>
+            <h3>Conservation Mission</h3>
+            <p>{props.cardData.conservationMission}</p>
+          </div>
         </div>
-        <div>
-          <h3>Age</h3>
-          <p>
-            {props.cardData.dateOfBirth !== "" ? calculateAge(props.cardData.dateOfBirth) : "Unknown"}
-          </p>
+        <div className={styles.back}>
+            {props.close ? (
+              <div className={styles.close} onClick={props.close}>&times;</div>
+            ) : null}
+
+            <h2>Help {props.cardData.name.split(" ")[0]}'s Species</h2>
+            <ul>
+              <li>Exmaple 1</li>
+              <li>Exmaple 2</li>
+              <li>Exmaple 3</li>
+              <li>Exmaple 4</li>
+              <li>Exmaple 5</li>
+            </ul>
         </div>
-        <div>
-          <h3>Birthday</h3>
-          <p>
-            {props.cardData.dateOfBirth !== "" ? formatDate(props.cardData.dateOfBirth) : "Unknown"}
-          </p>
-        </div>
-      </div>
-
-      <div className={styles.row}>
-        <h3>IUCN Status</h3>
-        <p>{props.cardData.iucnStatus}</p>
-      </div>
-
-      <div className={`${styles.row} ${styles.story}`}>
-        <h3>Story</h3>
-        <p>{props.cardData.story}</p>
-      </div>
-
-      <div className={`${styles.row} ${styles.conservationMission}`}>
-        <h3>Conservation Mission</h3>
-        <p>{props.cardData.conservationMission}</p>
-      </div>
-    </Ambassador>
+      </Ambassador>
+      <button onClick={()=>setIsFlipped(!isFlipped)}>{!isFlipped ? "Help "+ props.cardData.name.split(" ")[0] + "'s Species" : "About "+props.cardData.name.split(" ")[0]}</button>
+    </div>
   )
 }

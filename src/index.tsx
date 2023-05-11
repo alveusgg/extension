570e45ed475f76acb8c21bd0db38e44ca2b354df
MattIPv4 <me@mattcowley.co.uk>
2023-05-11 03:17:16 +0100

    Refactor how we expose Twitch auth

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import {registerTwitchAuthCallback} from './utils/channels'
+
 import App from './pages/panel/components/App';
 import reportWebVitals from './reportWebVitals';
+import { bindTwitchAuth } from './utils/twitch-api';
 
-registerTwitchAuthCallback()
+import './index.css';
 
-const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement)
+bindTwitchAuth();
+
+const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 
 root.render(
   <React.StrictMode>
--- src/pages/overlay/index.tsx
+++ src/pages/overlay/index.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-import {registerTwitchAuthCallback} from '../../utils/channels'
-import App from './App'
+import App from './App';
+import { bindTwitchAuth } from '../../utils/twitch-api';
 
 import './index.css';
 
-registerTwitchAuthCallback()
+bindTwitchAuth();
 
-const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement)
+const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 
 root.render(
   <React.StrictMode>

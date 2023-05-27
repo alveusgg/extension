092437eb94457347499f3c33f9e6ec8bb386e107
MattIPv4 <me@mattcowley.co.uk>
2023-05-27 23:52:15 +0100

    Cleanup DisableChatPopup implementation

--- src/pages/overlay/components/disableChatPopup/DisableChatPopup.tsx
+++ src/pages/overlay/components/disableChatPopup/DisableChatPopup.tsx
@@ -1,4 +1,3 @@
-// css
 import styles from './disableChatPopup.module.css'
 
 interface DisableChatPopupProps {
@@ -9,7 +8,10 @@ interface DisableChatPopupProps {
 export default function DisableChatPopup(props: DisableChatPopupProps) {
   return (
     <div className={styles.switchContainer}>
-      <label>Popups {props.disableChatPopup ? "disabled" : "enabled"}</label>
+      <label>
+        Popups <br/>
+        {props.disableChatPopup ? "disabled" : "enabled"}
+      </label>
       <label className={styles.switch}>
         <input type="checkbox" onChange={props.toggleDisableChatPopup} checked={!props.disableChatPopup} />
         <span className={styles.slider}></span>

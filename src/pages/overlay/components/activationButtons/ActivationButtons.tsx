7d78fc22459a72d2b9ab1743100dd9926d0b328d
MattIPv4 <me@mattcowley.co.uk>
2023-05-27 22:38:42 +0100

    Cleanup ActivationButtons implementation

--- src/pages/overlay/components/activationButtons/ActivationButtons.tsx
+++ src/pages/overlay/components/activationButtons/ActivationButtons.tsx
@@ -1,11 +1,8 @@
-//css & assets
-import styles from './activationButtons.module.css'
 import Welcome from '../../../../assets/activationButtons/welcome.jpg';
 import Ambassadors from '../../../../assets/activationButtons/ambassadors.jpg';
 
-/**
- * @Description Activation buttons are buttons that are used to show parts of the overlay.
- */
+import styles from './activationButtons.module.css'
+
 interface ActivationButtonProps {
   toggleShowAlveusIntro: () => void;
   toggleShowAmbassadorList: () => void;

9970880d577b197d70366de78674f8fa76921ee5
jibranabsarulislam <jga26@njit.edu>
2023-06-04 17:39:15 -0400

    add tooltip using tailwindcss button params

--- src/pages/overlay/components/buttons/Buttons.tsx
+++ src/pages/overlay/components/buttons/Buttons.tsx
@@ -4,7 +4,7 @@ import { classes } from '../../../../utils/classes'
 
 import styles from './buttons.module.scss'
 
-type ButtonsOptions = Readonly<{ key: string, title: string, type: "primary" | "secondary", icon: string }[]>;
+type ButtonsOptions = Readonly<{ key: string, title: string, type: "primary" | "secondary", icon: string, hoverText: string }[]>;
 
 interface ButtonsProps<T extends ButtonsOptions> {
   options: T;
@@ -32,6 +32,8 @@ export default function Buttons<T extends ButtonsOptions = ButtonsOptions>(props
           key={option.key}
           onClick={option.onClick}
           className={classes(option.active && styles.highlighted, option.type === "secondary" && styles.secondary)}
+          data-te-toggle="tooltip"
+          title={option.hoverText}
         >
           <img src={option.icon} alt={option.title} />
         </button>

c4c6d9983ab46bccc09fd67270acbaf13dd7376d
jibranabsarulislam <jga26@njit.edu>
2023-06-08 14:32:12 -0400

    implement useref to determine correct positioning

--- src/utils/global/tooltip/Tooltip.tsx
+++ src/utils/global/tooltip/Tooltip.tsx
@@ -1,40 +1,62 @@
-import React, { useState } from 'react'
+import React, { useRef, useState } from 'react'
 
 import styles from './tooltip.module.scss'
+import { classes } from '../../classes'
 
 interface TooltipProps {
   text: string;
+  ariaLabel: string;
   children: React.ReactNode;
-  title: string;
+  className?: string;
+  fontSize?: string;
   textContainerWidth?: string;
-  leftOffset?: string;
 }
 
 const Tooltip: React.FC<TooltipProps> = (props) => {
   const [show, setShow] = useState(false);
 
-  const tooltipStyle: React.CSSProperties = {
+  const containerRef = useRef<HTMLDivElement>(null);
+  const tooltipRef = useRef<HTMLDivElement>(null);
+
+  // use ref and calculate position on render to determine where to place tooltip
+  const containerRect = containerRef.current?.getBoundingClientRect();
+  const tooltipRect = tooltipRef.current?.getBoundingClientRect();
+
+  const containerCenter = (containerRect?.top ?? 0) + (containerRect?.height ?? 0) / 2;
+  
+  // set tooltip style based on incoming props
+  const tooltipStyle = {
     opacity: show ? 1 : 0,
-    width: props.textContainerWidth ? props.textContainerWidth : "max-content",
-    left: props.leftOffset,
-  };
+    // restrict width of tooltip with optional prop
+    width: props.textContainerWidth ? props.textContainerWidth : 'auto',
+    fontSize: props.fontSize ? props.fontSize : '1rem',
+  }
 
   return (
-    <>
+    <div
+      className={props.className}
+      ref={containerRef}
+      aria-label={props.ariaLabel}
+    >
       <div
-        className={styles.wrapper}
-        aria-label={props.title}
         onMouseEnter={() => setShow(true)}
         onMouseLeave={() => setShow(false)}
+        className={classes(props.className, styles.children)}
       >
-        <div className={styles.children}>{props.children}</div>
-        <div className={styles.sharedStyles} style={tooltipStyle}>
-          <span className={styles.triangle} />
-          {props.text}
-        </div>
+        {props.children}
       </div>
-    </>
-  );
+      <div className={styles.tooltip}
+        ref={tooltipRef}
+        style={{
+          ...tooltipStyle,
+          top: containerCenter - (tooltipRect?.height ?? 0) / 2,
+          left: (containerRef.current?.getBoundingClientRect().right ?? 0) + 10,
+        }}>
+        <span className={styles.triangle} />
+        {props.text}
+      </div>
+    </div >
+  )
 };
 
 export default Tooltip;

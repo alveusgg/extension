fe84c80e862dda43dc671e5b3f4054a8eb4efbee
jayway <70596906+jibranabsarulislam@users.noreply.github.com>
2023-06-08 19:01:54 -0400

    add comment and remove cursor property instead of setting it
    Co-authored-by: Matt Cowley <me@mattcowley.co.uk>

--- src/pages/overlay/App.tsx
+++ src/pages/overlay/App.tsx
@@ -93,11 +93,11 @@ export default function App() {
     wake(5000)
   }, [wake])
 
+  // Hide the cursor when sleeping
   useEffect(() => {
     if (sleeping) document.documentElement.style.cursor = 'none'
-    else document.documentElement.style.cursor = 'default'
+    else document.documentElement.style.removeProperty('cursor')
   }, [sleeping])
-
 
   // When a user scrolls, treat it as an interaction (but handle Firefox being weird)
   const scrollRef = useRef<[HTMLElement, number]|undefined>(undefined)

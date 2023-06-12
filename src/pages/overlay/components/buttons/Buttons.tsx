dad77749b490c83209d63af710761aa0bbfa2529
Matt Cowley <me@mattcowley.co.uk>
2023-06-12 23:13:13 +0100

    Make tooltips accessible, avoid wrapper element (#108)
    * Tooltips should have an accessible id
    
    * Avoid needing to wrap tooltip component in div

--- src/pages/overlay/components/buttons/Buttons.tsx
+++ src/pages/overlay/components/buttons/Buttons.tsx
@@ -32,16 +32,12 @@ export default function Buttons<T extends ButtonsOptions = ButtonsOptions>(props
         <Tooltip
           key={option.key}
           text={option.hoverText}
-          ariaLabel={option.title}
-          className={classes(
-            styles.btn,
-            option.type === "secondary" && styles.secondary
-          )}
         >
           <button
-            key={option.key}
             onClick={option.onClick}
             className={classes(
+              styles.btn,
+              option.type === "secondary" && styles.secondary,
               option.active && styles.highlighted,
             )}
           >

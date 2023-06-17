7c4299760ed7e838acc61dd5b05b388d5dd0367d
MattIPv4 <me@mattcowley.co.uk>
2023-06-17 23:34:24 +0100

    Move scroll detection to hook, require user event

--- src/pages/overlay/App.tsx
+++ src/pages/overlay/App.tsx
@@ -1,9 +1,10 @@
-import React, { useEffect, useCallback, useRef } from "react";
+import React, { useEffect, useCallback } from "react";
 
 import { classes } from "../../utils/classes";
 import useHiddenCursor from "./hooks/useHiddenCursor";
 import useSettings from "./hooks/useSettings";
 import useSleeping from "./hooks/useSleeping";
+import useScrolling from "./hooks/useScrolling";
 
 import Overlay from "./components/overlay/Overlay";
 import styles from "./App.module.scss";
@@ -20,7 +21,6 @@ export default function App() {
     on: addSleepListener,
     off: removeSleepListener,
   } = useSleeping();
-  const appRef = useRef<HTMLDivElement>(null);
 
   // When the user interacts, show the overlay
   const interacted = useCallback(() => wake(timeout), [wake]);
@@ -34,32 +34,16 @@ export default function App() {
     return () => removeSleepListener("wake", showCursor);
   }, [addSleepListener, removeSleepListener, showCursor]);
 
-  // When a user scrolls, treat it as an interaction (but handle Firefox being weird)
-  const scrollRef = useRef<[HTMLElement, number] | undefined>(undefined);
-  const scrolled = useCallback(
-    (e: Event) => {
-      const target = e.target as HTMLElement;
-
-      // If the scroll event is from the same element, and the scroll position is the same, ignore it
-      // This fixes a bug in Firefox where animating translate emits scroll events
-      if (scrollRef.current) {
-        const [node, scrollTop] = scrollRef.current;
-        if (node === target && scrollTop === target.scrollTop) return;
-      }
-
-      scrollRef.current = [target, target.scrollTop];
-      interacted();
-    },
-    [interacted]
-  );
-
-  // Bind a capturing event listener for scrolling (so we can see scrolling for children)
+  // When a user scrolls, treat it as an interaction
+  const {
+    ref: scrollRef,
+    on: addScrollListener,
+    off: removeScrollListener,
+  } = useScrolling();
   useEffect(() => {
-    if (!appRef.current) return;
-    const node = appRef.current;
-    node.addEventListener("scroll", scrolled, true);
-    return () => node.removeEventListener("scroll", scrolled, true);
-  }, [scrolled]);
+    addScrollListener(interacted);
+    return () => removeScrollListener(interacted);
+  }, [addScrollListener, removeScrollListener, interacted]);
 
   // Block sleeping hiding the overlay if dev toggle is on
   const settings = useSettings();
@@ -72,7 +56,7 @@ export default function App() {
 
   return (
     <div
-      ref={appRef}
+      ref={scrollRef}
       className={classes(styles.app, visibilityClass)}
       onMouseEnter={interacted}
       onMouseMove={interacted}

1d612653dafb466f5c201c0c36cf913d4a13de03
Sylvia Han <sylviacodes@gmail.com>
2024-01-31 10:28:18 -0800

    [Panel] Show empty welcome card when clicking on info button in nav bar

--- src/pages/panel/App.tsx
+++ src/pages/panel/App.tsx
@@ -2,12 +2,20 @@ import Nav from "./components/nav/Nav";
 import AmbassadorPanel from "./components/ambassadorPanel/AmbassadorPanel";
 
 import "./App.scss";
+import { useState } from "react";
+import WelcomeCard from "./components/welcomeCard/WelcomeCard";
 
 function App() {
+  const [shouldShowWelcomeCard, setShouldShowWelcomeCard] =
+    useState<boolean>(false);
+
   return (
     <div className="App">
-      <Nav />
+      <Nav onShowWelcomeCardClicked={() => setShouldShowWelcomeCard(true)} />
       <AmbassadorPanel />
+      {shouldShowWelcomeCard && (
+        <WelcomeCard onClose={() => setShouldShowWelcomeCard(false)} />
+      )}
     </div>
   );
 }
--- src/pages/panel/components/nav/Nav.tsx
+++ src/pages/panel/components/nav/Nav.tsx
@@ -2,11 +2,25 @@ import alveus from "../../../../assets/alveus.png";
 
 import styles from "./nav.module.scss";
 
-export default function Nav() {
+import IconInfo from "../../../../components/icons/IconInfo";
+
+interface NavProps {
+  onShowWelcomeCardClicked: () => void;
+}
+
+export default function Nav({ onShowWelcomeCardClicked }: NavProps) {
   return (
     <nav className={styles.nav}>
+      <div className={styles.filler} />
       <img src={alveus} alt="Alveus Logo" />
       <h1>Alveus Ambassadors</h1>
+      <button
+        className={styles.info}
+        onClick={() => onShowWelcomeCardClicked()}
+        title="Info"
+      >
+        <IconInfo size="20" />
+      </button>
     </nav>
   );
 }

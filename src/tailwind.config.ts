0aa2b24daa8db644b089199f48423a66a67e5eaa
MattIPv4 <me@mattcowley.co.uk>
2024-10-23 22:25:18 +0100

    Migrate panel AmbassadorPanel component styles

--- src/tailwind.config.ts
+++ src/tailwind.config.ts
@@ -1,6 +1,7 @@
 import { type Config } from "tailwindcss";
 import { fontFamily } from "tailwindcss/defaultTheme";
 import alveusgg from "@alveusgg/data/src/tailwind";
+import scrollbar from "tailwind-scrollbar";
 
 const config = {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
@@ -12,6 +13,7 @@ const config = {
       },
     },
   },
+  plugins: [scrollbar],
 } satisfies Config;
 
 export default config;

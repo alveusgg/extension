30f49e9294cd22d064aeeb4920f9a53520392233
MattIPv4 <me@mattcowley.co.uk>
2024-10-23 21:38:34 +0100

    Migrate panel WelcomeCardOverlay component styles

--- src/pages/panel/App.tsx
+++ src/pages/panel/App.tsx
@@ -1,19 +1,11 @@
-import { useState } from "react";
-
 import Nav from "./components/nav/Nav";
 import AmbassadorPanel from "./components/ambassadorPanel/AmbassadorPanel";
-import WelcomeCardOverlay from "./components/welcomeCardOverlay/WelcomeCardOverlay";
 
 function App() {
-  const [showWelcome, setShowWelcome] = useState<boolean>(false);
-
   return (
     <div className="relative h-full w-full">
-      <Nav onWelcomeClick={() => setShowWelcome(true)} />
+      <Nav />
       <AmbassadorPanel />
-      {showWelcome && (
-        <WelcomeCardOverlay onClose={() => setShowWelcome(false)} />
-      )}
     </div>
   );
 }
--- src/pages/panel/components/nav/Nav.tsx
+++ src/pages/panel/components/nav/Nav.tsx
@@ -1,23 +1,31 @@
+import { useState } from "react";
+
 import alveus from "../../../../assets/alveus.png";
 
 import styles from "./nav.module.scss";
 
 import IconInfo from "../../../../components/icons/IconInfo";
+import WelcomeCardOverlay from "../welcomeCardOverlay/WelcomeCardOverlay";
 
-interface NavProps {
-  onWelcomeClick: () => void;
-}
-
-export default function Nav(props: NavProps) {
-  const { onWelcomeClick } = props;
+export default function Nav() {
+  const [showWelcome, setShowWelcome] = useState<boolean>(false);
 
   return (
     <nav className={styles.nav}>
       <img src={alveus} alt="Alveus Logo" />
       <h1>Alveus Ambassadors</h1>
-      <button className={styles.info} onClick={onWelcomeClick} title="Info">
+      <button
+        className={styles.info}
+        onClick={() => setShowWelcome(true)}
+        title="Info"
+      >
         <IconInfo size="20" />
       </button>
+
+      <WelcomeCardOverlay
+        show={showWelcome}
+        onClose={() => setShowWelcome(false)}
+      />
     </nav>
   );
 }

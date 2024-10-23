377edff5dc999fb8ef2244ad33eac65f10de954e
MattIPv4 <me@mattcowley.co.uk>
2024-10-23 22:02:32 +0100

    Migrate panel Nav component styles

--- src/pages/panel/components/nav/Nav.tsx
+++ src/pages/panel/components/nav/Nav.tsx
@@ -2,8 +2,6 @@ import { useState } from "react";
 
 import alveus from "../../../../assets/alveus.png";
 
-import styles from "./nav.module.scss";
-
 import IconInfo from "../../../../components/icons/IconInfo";
 import WelcomeCardOverlay from "../welcomeCardOverlay/WelcomeCardOverlay";
 
@@ -11,15 +9,18 @@ export default function Nav() {
   const [showWelcome, setShowWelcome] = useState<boolean>(false);
 
   return (
-    <nav className={styles.nav}>
-      <img src={alveus} alt="Alveus Logo" />
-      <h1>Alveus Ambassadors</h1>
+    <nav className="bg-alveus-green/85 fixed inset-x-0 top-0 z-10 flex h-12 w-full items-center justify-start gap-3 px-4 shadow-lg backdrop-blur sm:justify-center">
+      <img className="h-8 w-auto" src={alveus} alt="Alveus Logo" />
+      <h1 className="text-lg">Alveus Ambassadors</h1>
       <button
-        className={styles.info}
+        className="group -mr-2 ml-auto rounded-full p-2 sm:absolute sm:right-4 sm:ml-0"
         onClick={() => setShowWelcome(true)}
         title="Info"
       >
-        <IconInfo size="20" />
+        <IconInfo
+          size={20}
+          className="outline-highlight rounded-full transition-[outline] group-hover:outline"
+        />
       </button>
 
       <WelcomeCardOverlay

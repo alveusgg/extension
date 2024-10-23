e219283882420288c59cc536dab0288ac9fac0c7
MattIPv4 <me@mattcowley.co.uk>
2024-10-23 21:02:58 +0100

    Abstract more Tailwind config settings

--- src/tailwind.config.ts
+++ src/tailwind.config.ts
@@ -7,20 +7,8 @@ const config = {
   presets: [alveusgg],
   theme: {
     extend: {
-      colors: {
-        highlight: "#FF9F1C",
-      },
       fontFamily: {
         sans: ["Nunito", ...fontFamily.sans],
-      },
-      // TODO: Abstract into alveusgg/data
-      gridTemplateColumns: {
-        ...Object.fromEntries(
-          Array.from({ length: 12 }, (_, i) => [
-            `${i + 1}-auto`,
-            `repeat(${i + 1}, auto)`,
-          ]),
-        ),
       },
     },
   },

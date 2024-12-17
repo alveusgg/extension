d4a8f927222a1e7540a7cf8afb6aa801ef332fc7
Guilherme Andrade <guilherme.andrade.ao@gmail.com>
2024-12-17 01:26:37 +0100

    Upgrade Eslint to v9 (#204)
    * Upgrade eslint to v9
    
    * Fix webpack compilation by removing type: module and making eslintconfig a esmodule file. Clean up eslint command.
    
    * Enable and fix typescript issues in eslint.config
    
    * Make approach more consistent with alveusgg
    
    ---------
    
    Co-authored-by: Guilherme  Andrade <gandrade@remotelock.com>

---
+++ eslint.config.mjs
@@ -0,0 +1,40 @@
+// @ts-check
+import eslint from "@eslint/js";
+import tseslint from "typescript-eslint";
+import reactPlugin from "eslint-plugin-react";
+import prettiereslint from "eslint-config-prettier";
+
+export default tseslint.config(
+  {
+    name: "eslint/recommended",
+    ...eslint.configs.recommended,
+  },
+  ...tseslint.configs.recommended,
+  // @ts-expect-error - incorrect types
+  reactPlugin.configs.flat.recommended,
+  {
+    name: "prettier/config",
+    ...prettiereslint,
+  },
+  {
+    languageOptions: {
+      parserOptions: {
+        ecmaVersion: 2021,
+        sourceType: "module",
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+    },
+    settings: {
+      react: {
+        version: "detect",
+      },
+    },
+    rules: {
+      "@typescript-eslint/no-explicit-any": "off",
+      "react/jsx-uses-react": "off",
+      "react/react-in-jsx-scope": "off",
+    },
+  },
+);

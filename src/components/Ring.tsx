b34cf160be6b07a04610381faf2f49be1465d6d1
MattIPv4 <me@mattcowley.co.uk>
2025-04-18 03:36:24 +0100

    Add light ring to cards

--- src/components/Ring.tsx
+++ src/components/Ring.tsx
@@ -2,16 +2,19 @@ import { classes } from "../utils/classes";
 
 interface RingProps {
   active?: boolean;
+  className?: string;
 }
 
-export default function Ring({ active = false }: RingProps) {
+export default function Ring({ active = false, className }: RingProps) {
   return (
     <div
       className={classes(
-        "pointer-events-none absolute inset-0 rounded-lg ring-3 transition-shadow ring-inset",
+        "pointer-events-none absolute inset-0 ring-3 transition-shadow ring-inset",
         active
           ? "ring-highlight"
           : "ring-white/25 group-hover/button:ring-highlight group-focus/button:ring-highlight",
+        !/\brounded-/.test(className || "") && "rounded-lg",
+        className,
       )}
     />
   );

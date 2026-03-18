65f864597b297ca5988cb592780f843c59dea6fa
MattIPv4 <me@mattcowley.co.uk>
2026-03-18 11:27:36 +0100

    Remove cross-env for Webpack config

--- webpack.config.ts
+++ webpack.config.ts
@@ -12,13 +12,6 @@ import ZipWebpackPlugin from "zip-webpack-plugin";
 
 import "webpack-dev-server";
 
-const isDev = process.env.NODE_ENV === "development";
-
-const env = {
-  ...dotenv.config({ path: "./.env" }).parsed,
-  ...(isDev && dotenv.config({ path: "./.env.development" }).parsed),
-};
-
 class UnusedAssetsPlugin {
   apply(compiler: webpack.Compiler) {
     if (compiler.options.mode && compiler.options.mode !== "production") return;
@@ -75,7 +68,7 @@ class UnusedAssetsPlugin {
   }
 }
 
-const getTypeScriptLoader = () => ({
+const getTypeScriptLoader = (isDev: boolean) => ({
   loader: "ts-loader",
   options: {
     getCustomTransformers: () => ({
@@ -85,7 +78,7 @@ const getTypeScriptLoader = () => ({
   },
 });
 
-const getStyleLoader = () => [
+const getStyleLoader = (isDev: boolean) => [
   isDev
     ? "style-loader"
     : {
@@ -112,173 +105,185 @@ const getStyleLoader = () => [
   },
 ];
 
-const config: webpack.Configuration = {
-  // Set the mode based on the environment
-  mode: isDev ? "development" : "production",
-  devtool: isDev ? "cheap-module-source-map" : "source-map",
-  // Create a development server (using https to allow chat connection)
-  devServer: {
-    open: true,
-    hot: true,
-    server: {
-      type: "https",
+type Env = {
+  [key: string]: true | string | Env;
+};
+
+const config = (env: Env): webpack.Configuration => {
+  const isDev = !!env.WEBPACK_SERVE;
+  const envVars = {
+    ...dotenv.config({ path: "./.env" }).parsed,
+    ...(isDev && dotenv.config({ path: "./.env.development" }).parsed),
+  };
+
+  return {
+    // Set the mode based on the environment
+    mode: isDev ? "development" : "production",
+    devtool: isDev ? "cheap-module-source-map" : "source-map",
+    // Create a development server (using https to allow chat connection)
+    devServer: {
+      open: true,
+      hot: true,
+      server: {
+        type: "https",
+      },
+      client: {
+        overlay: {
+          errors: true,
+          warnings: true,
+          // No overlay for runtime errors, like chat connections being killed
+          runtimeErrors: false,
+        },
+      },
     },
-    client: {
-      overlay: {
-        errors: true,
-        warnings: true,
-        // No overlay for runtime errors, like chat connections being killed
-        runtimeErrors: false,
-      },
+    // Don't watch node_modules in development (too many files)
+    // FIXME: https://github.com/TypeStrong/fork-ts-checker-webpack-plugin/issues/859
+    watchOptions: {
+      ignored: /node_modules/,
     },
-  },
-  // Don't watch node_modules in development (too many files)
-  // FIXME: https://github.com/TypeStrong/fork-ts-checker-webpack-plugin/issues/859
-  watchOptions: {
-    ignored: /node_modules/,
-  },
-  // We need three entry points, and three outputs w/ html
-  entry: {
-    panel: "./src/pages/panel/index.tsx",
-    mobile: "./src/pages/panel/index.tsx",
-    overlay: "./src/pages/overlay/index.tsx",
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: "src/template.html",
-      filename: "panel.html",
-      chunks: ["panel"],
-      templateParameters: {
-        process: {
-          env,
-        },
-      },
-    }),
-    new HtmlWebpackPlugin({
-      template: "src/template.html",
-      filename: "mobile.html",
-      chunks: ["mobile"],
-      templateParameters: {
-        process: {
-          env,
-        },
-      },
-    }),
-    new HtmlWebpackPlugin({
-      template: "src/template.html",
-      filename: "video_overlay.html",
-      chunks: ["overlay"],
-      templateParameters: {
-        process: {
-          env,
-        },
-      },
-    }),
-    // Include the public directory
-    new CopyWebpackPlugin({
-      patterns: [{ from: "public", to: "." }],
-    }),
-    // Load environment variables as a single object
-    new webpack.DefinePlugin({
-      "process.env": JSON.stringify(env),
-    }),
-    // Enforce type checking on a separate process
-    new ForkTsCheckerWebpackPlugin(),
-    // Remove any unused assets after code optimization
-    new UnusedAssetsPlugin(),
-    // Enable react hot reloading in development
-    isDev &&
-      new ReactRefreshWebpackPlugin({
-        overlay: false,
-      }),
-    // Dedicated CSS files in production
-    !isDev &&
-      new MiniCssExtractPlugin({
-        filename: "static/css/[name].[contenthash:8].css",
-        chunkFilename: "static/css/[name].[contenthash:8].chunk.css",
-      }),
-    // Zip the build for easy distribution
-    !isDev &&
-      new ZipWebpackPlugin({
-        filename: "build.zip",
-      }),
-  ].filter(Boolean),
-  output: {
-    path: join(process.cwd(), "build"),
-    filename: isDev
-      ? "static/js/[name].js"
-      : "static/js/[name].[contenthash:8].js",
-    chunkFilename: isDev
-      ? "static/js/[name].chunk.js"
-      : "static/js/[name].[contenthash:8].chunk.js",
-    assetModuleFilename: "static/media/[name].[hash][ext]",
-    publicPath: isDev ? "auto" : "./",
-    clean: !isDev,
-  },
-  // Ensure compatibility based on browserslist
-  target: "browserslist",
-  // Setup loaders for all our file types
-  module: {
-    rules: [
-      // Load typescript
-      {
-        test: /\.tsx?$/,
-        exclude: /node_modules/,
-        use: getTypeScriptLoader(),
-      },
-      // Load tailwind
-      {
-        test: /\.css$/i,
-        exclude: /node_modules/,
-        use: getStyleLoader(),
-      },
-      // Load images
-      {
-        test: /\.(png|jpe?g|gif|svg)$/i,
-        exclude: /node_modules/,
-        type: "asset",
-      },
-      // Load ambassador images in @alveusgg packages
-      {
-        test: /\.(png|jpe?g)$/,
-        include: [
-          new RegExp(
-            join(
-              "node_modules",
-              "@alveusgg",
-              "data",
-              "build",
-              "assets",
-              "ambassadors",
-            ).replace(/\\/g, "\\\\"),
-          ),
-          new RegExp(join("src", "assets", "winston").replace(/\\/g, "\\\\")),
-        ],
-        type: "asset",
-        generator: {
-          filename: (pathData) => {
-            if (!pathData.filename) return "";
-            const dir = basename(dirname(pathData.filename));
-            return `static/media/ambassadors/${dir}/[name].[contenthash][ext]`;
+    // We need three entry points, and three outputs w/ html
+    entry: {
+      panel: "./src/pages/panel/index.tsx",
+      mobile: "./src/pages/panel/index.tsx",
+      overlay: "./src/pages/overlay/index.tsx",
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: "src/template.html",
+        filename: "panel.html",
+        chunks: ["panel"],
+        templateParameters: {
+          process: {
+            env: envVars,
           },
-        } as webpack.WebpackOptionsNormalized["module"]["generator"]["asset"],
-        use: [
-          {
-            loader: "webpack-image-resize-loader",
-            options: {
-              width: 550,
+        },
+      }),
+      new HtmlWebpackPlugin({
+        template: "src/template.html",
+        filename: "mobile.html",
+        chunks: ["mobile"],
+        templateParameters: {
+          process: {
+            env: envVars,
+          },
+        },
+      }),
+      new HtmlWebpackPlugin({
+        template: "src/template.html",
+        filename: "video_overlay.html",
+        chunks: ["overlay"],
+        templateParameters: {
+          process: {
+            env: envVars,
+          },
+        },
+      }),
+      // Include the public directory
+      new CopyWebpackPlugin({
+        patterns: [{ from: "public", to: "." }],
+      }),
+      // Load environment variables as a single object
+      new webpack.DefinePlugin({
+        "process.env": JSON.stringify(envVars),
+      }),
+      // Enforce type checking on a separate process
+      new ForkTsCheckerWebpackPlugin(),
+      // Remove any unused assets after code optimization
+      new UnusedAssetsPlugin(),
+      // Enable react hot reloading in development
+      isDev &&
+        new ReactRefreshWebpackPlugin({
+          overlay: false,
+        }),
+      // Dedicated CSS files in production
+      !isDev &&
+        new MiniCssExtractPlugin({
+          filename: "static/css/[name].[contenthash:8].css",
+          chunkFilename: "static/css/[name].[contenthash:8].chunk.css",
+        }),
+      // Zip the build for easy distribution
+      !isDev &&
+        new ZipWebpackPlugin({
+          filename: "build.zip",
+        }),
+    ].filter(Boolean),
+    output: {
+      path: join(process.cwd(), "build"),
+      filename: isDev
+        ? "static/js/[name].js"
+        : "static/js/[name].[contenthash:8].js",
+      chunkFilename: isDev
+        ? "static/js/[name].chunk.js"
+        : "static/js/[name].[contenthash:8].chunk.js",
+      assetModuleFilename: "static/media/[name].[hash][ext]",
+      publicPath: isDev ? "auto" : "./",
+      clean: !isDev,
+    },
+    // Ensure compatibility based on browserslist
+    target: "browserslist",
+    // Setup loaders for all our file types
+    module: {
+      rules: [
+        // Load typescript
+        {
+          test: /\.tsx?$/,
+          exclude: /node_modules/,
+          use: getTypeScriptLoader(!!isDev),
+        },
+        // Load tailwind
+        {
+          test: /\.css$/i,
+          exclude: /node_modules/,
+          use: getStyleLoader(!!isDev),
+        },
+        // Load images
+        {
+          test: /\.(png|jpe?g|gif|svg)$/i,
+          exclude: /node_modules/,
+          type: "asset",
+        },
+        // Load ambassador images in @alveusgg packages
+        {
+          test: /\.(png|jpe?g)$/,
+          include: [
+            new RegExp(
+              join(
+                "node_modules",
+                "@alveusgg",
+                "data",
+                "build",
+                "assets",
+                "ambassadors",
+              ).replace(/\\/g, "\\\\"),
+            ),
+            new RegExp(join("src", "assets", "winston").replace(/\\/g, "\\\\")),
+          ],
+          type: "asset",
+          generator: {
+            filename: (pathData) => {
+              if (!pathData.filename) return "";
+              const dir = basename(dirname(pathData.filename));
+              return `static/media/ambassadors/${dir}/[name].[contenthash][ext]`;
             },
-          },
-        ],
-      },
-    ],
-  },
-  resolve: {
-    extensions: [".tsx", ".ts", ".jsx", ".js"],
-  },
-  optimization: {
-    minimize: !isDev,
-  },
+          } as webpack.WebpackOptionsNormalized["module"]["generator"]["asset"],
+          use: [
+            {
+              loader: "webpack-image-resize-loader",
+              options: {
+                width: 550,
+              },
+            },
+          ],
+        },
+      ],
+    },
+    resolve: {
+      extensions: [".tsx", ".ts", ".jsx", ".js"],
+    },
+    optimization: {
+      minimize: !isDev,
+    },
+  };
 };
 
 export default config;
